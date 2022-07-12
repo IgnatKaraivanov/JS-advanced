f8db9170b2df180299648ee75addb1d9aadd5e61
IgnatKaraivanov <103810206+IgnatKaraivanov@users.noreply.github.com>
2022-07-12 06:30:53 +0300

    Add files via upload

---
+++ 01 Syntax, Functions and Statements/Exercise/class.js
@@ -0,0 +1,12 @@
+class Citylog{
+    constructor(city,code){
+    this.city=city;
+    this.code=code;
+
+    }
+}
+var firstCity= new Citylog('Sofia','1000');
+var secondCity= new Citylog('Mofia','4000');
+
+console.log(firstCity);
+console.log(secondCity);
