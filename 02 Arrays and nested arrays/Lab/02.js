ed9b1eee4905f9dee8c8a58f2cb1fb79cd319715
IgnatKaraivanov <103810206+IgnatKaraivanov@users.noreply.github.com>
2022-07-14 06:59:01 +0300

    Add files via upload

--- 02 Arrays and nested arrays/Lab/02.js
+++ 02 Arrays and nested arrays/Lab/02.js
@@ -1,29 +1,17 @@
-function citiesPopulation(list) {
-  const result = {};
-  for (const town of list) {
-    let [city, population] = town.split(" <-> ");
-    population = Number(population);
-    if (result[city] != undefined) {
-      population += result[city];
+function solve(arr, element) {
+    let result = [];
+    for (let i = 0; i < arr.length; i += element) {
+        result.push(arr[i]);
     }
-    result[city] = population;
-  }
-  for (const every in result) {
-    console.log(`${every} : ${result[every]}`);
-  }
+    
+    return result;
+
 }
-citiesPopulation([
-  "Sofia <-> 1200000",
-  "Montana <-> 20000",
-  "New York <-> 10000000",
-  "Washington <-> 2345000",
-  "Las Vegas <-> 1000000",
-]);
-console.log("-----");
-citiesPopulation([
-  "Istanbul <-> 100000",
-  "Honk Kong <-> 2100004",
-  "Jerusalem <-> 2352344",
-  "Mexico City <-> 23401925",
-  "Istanbul <-> 1000",
-]);
+
+solve(['5',
+    '20',
+    '31',
+    '4',
+    '20'],
+    2
+)
