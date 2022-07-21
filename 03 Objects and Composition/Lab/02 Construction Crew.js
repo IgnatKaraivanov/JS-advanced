9ce818589973a67c88769f521303bdea781a5f7d
IgnatKaraivanov <103810206+IgnatKaraivanov@users.noreply.github.com>
2022-07-21 06:32:02 +0300

    Add files via upload

---
+++ 03 Objects and Composition/Lab/02 Construction Crew.js
@@ -0,0 +1,11 @@
+function solve(worker) {
+ if(worker.dizziness){
+     worker.levelOfHydrated += 0.1 * worker.experience * worker.weight;
+     worker.dizziness=false
+ }
+ return worker;
+}
+solve({ weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true })
