430e254b92220bd9b27a1875aaa981a5d1e52dd0
IgnatKaraivanov <103810206+IgnatKaraivanov@users.noreply.github.com>
2022-07-23 06:38:11 +0300

    Add files via upload

---
+++ 03 Objects and Composition/Lab/06 7. Store Catalogue.js
@@ -0,0 +1,31 @@
+function solve(array) {
+    let result = array.sort((a, b) => a.localeCompare(b));
+    let current = [];
+    let firstChar = '';
+
+    for (let index = 0; index < result.length; index++) {
+        current = result[index].split(' : ');
+        if (result[index][0] !== firstChar) {
+            console.log(result[index][0]);
+        }
+        console.log(`  ${current[0]}: ${current[1]}`);
+        firstChar = result[index][0]
+    }
+
+}
+solve(['Appricot : 20.4',
+    'Fridge : 1500',
+    'TV : 1499',
+    'Deodorant : 10',
+    'Boiler : 300',
+    'Apple : 1.25',
+    'Anti-Bug Spray : 15',
+    'T-Shirt : 10'])
+solve(['Banana : 2',
+    'Rubic\`s Cube : 5',
+    'Raspberry P : 4999',
+    'Rolex : 100000',
+    'Rollon : 10',
+    'Rali Car : 2000000',
+    'Pesho : 0.000001',
+    'Barrel : 10'])
