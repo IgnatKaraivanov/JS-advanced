97aeb11336c290dc0f1c08a89fe23c738002d192
IgnatKaraivanov <103810206+IgnatKaraivanov@users.noreply.github.com>
2022-08-16 07:30:51 +0300

    Add files via upload

---
+++ 08 Exercise Objects and Composition/3.Car Factory.js
@@ -0,0 +1,38 @@
+function carFactory(object) {
+  let resultCar = {};
+  let engine = {};
+  let carriage = {};
+  resultCar.model = object.model;
+  if (object.power <= 90) {
+    engine.power = 90;
+    engine.volume = 1800;
+  } else if (object.power <= 120) {
+    engine.power = 120;
+    engine.volume = 2400;
+  } else if (object.power <= 200) {
+    engine.power = 200;
+    engine.volume = 3500;
+  }
+  carriage.type = object.carriage;
+  carriage.color = object.color;
+  if (object.wheelsize % 2 === 0) {
+    object.wheelsize--;
+  }
+  resultCar.engine = engine;
+  resultCar.carriage = carriage;
+  resultCar.wheels = [
+    object.wheelsize,
+    object.wheelsize,
+    object.wheelsize,
+    object.wheelsize,
+  ];
+  console.log(resultCar);
+  return resultCar;
+}
+carFactory({
+  model: "VW Golf II",
+  power: 90,
+  color: "blue",
+  carriage: "hatchback",
+  wheelsize: 14,
+});
