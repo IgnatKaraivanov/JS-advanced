c61f6306e5bd76b93736589b56880dd757cc52ec
IgnatKaraivanov <103810206+IgnatKaraivanov@users.noreply.github.com>
2022-12-30 07:40:17 +0200

    Add files via upload

---
+++ 14 Exercise Advanced Functions/07. Simple Calculator/5.Functional Sum.js
@@ -0,0 +1,14 @@
+function add(num) {
+  let sum = 0;
+  function inner(number) {
+    sum += number;
+
+    return inner;
+  }
+  inner.toString = () => {
+    return sum;
+  };
+
+  return inner(num);
+}
+add(1)(6)(-3);
